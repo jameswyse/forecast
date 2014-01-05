70436f916412845f5247bbf30cbd3f2364100b18
James Wyse <james@jameswyse.net>
2014-01-06 03:20:43 +1000

    Add example

---
+++ example.js
@@ -0,0 +1,36 @@
+//
+// Forecast example
+//
+
+// Require the module
+var Forecast = require('./index.js');
+
+// Initialize with custom settings
+var forecast = new Forecast({
+  service: 'forecast.io',
+  key: 'your-api-key',
+  units: 'celcius',
+  cache: true,
+  ttl: {
+    minutes: 5
+  }
+});
+
+// Retrieve weather information using coordinates (Sydney, Australia)
+forecast.get([-33.8683, 151.2086], function(err, result) {
+  if(err) return console.dir(err);
+
+  console.log('Latitude: %s', result.latitude);
+  console.log('Longitude: %s', result.longitude);
+  console.log('Timezone: %s', result.timezone);
+  console.log();
+  console.log('Current Weather Conditions:');
+  console.log();
+  console.log(
+    ' %s, %s°C, %s% humidity',
+    result.currently.summary,
+    Math.round(parseFloat(result.currently.temperature, 10)),
+    result.currently.humidity * 100
+  );
+
+});
